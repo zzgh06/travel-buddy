efa1c2191e290506bfd51866c01e7546b8818586
zzgh06 <zzgh06@naver.com>
2024-12-05 16:54:18 +0900

    fix

--- src/components/TravelPlanList.tsx
+++ src/components/TravelPlanList.tsx
@@ -4,10 +4,10 @@ import { useState, useMemo } from 'react';
 import Link from 'next/link';
 import {
   ArrowRightCircleIcon,
-  CalendarDateRangeIcon,
+  CalendarIcon,
   MapIcon,
   PlusCircleIcon
-} from '@heroicons/react/16/solid';
+} from '@heroicons/react/24/solid';
 import TravelStatusFilter, { TravelStatus } from './FilterButton';
 import { useTravelPlans } from '@/hooks/useTravelPlan';
 import TravelPlanListSkeleton from './skeleton/TravelPlanListSkeleton';
@@ -113,7 +113,7 @@ export default function TravelPlanList() {
                   <span data-cy="trip-destination">{plan.destination}</span>
                 </div>
                 <div className='flex items-center text-gray-600 mb-4'>
-                  <CalendarDateRangeIcon className='w-5 h-5 mr-2' />
+                  <CalendarIcon className='w-5 h-5 mr-2' />
                   <span>
                     {new Date(plan.startDate).toLocaleDateString()} - {new Date(plan.endDate).toLocaleDateString()}
                   </span>
